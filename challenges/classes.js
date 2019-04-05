7b88f21e1f7a97867d94db9b975569e47160dd69
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-04-05 09:22:42 -0700

    finished most of the assignments. only need objects-arrays.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -5,3 +5,54 @@
 // console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+/* ===== Prototype Practice ===== */
+
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
+
+class CuboidMaker{
+    constructor(length,width,height){
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    }
+    volume(){
+    return this.width * this.height * this.length
+    }
+    surfaceArea(){
+    return 2 * (this.width * this.height +  this.height * this.length +  this.width * this.length);
+   
+    }
+}
+class CubeMaker extends CuboidMaker{
+    constructor(length,width,height){
+        super(length,width,height);
+          this.side = length;
+    }
+    surfaceArea(){
+        return 6 * this.side* this.side;
+    }
+    volume(){
+        return this.side*this.side*this.side;
+    }
+}
+   
+    
+    console.group('classes.js')
+
+    const cuboid = new CuboidMaker(4,5,5);
+    console.log(cuboid);
+    
+    // Test your volume and surfaceArea methods by uncommenting the logs below:
+    console.log(cuboid.volume()); // 100
+    console.log(cuboid.surfaceArea()); // 130
+    
+    const cube = new CubeMaker(5,5,5)
+    console.log(cube.volume());
+    console.log(cube.surfaceArea());
+    
+    console.groupEnd();
+    
+    
+    
+    
+    
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -49,8 +49,13 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
+const universities = []; 
+//graduates.map(element=> element.university).sort();
+for (let i = 0; i < graduates.length; i++) {
+ universities.push( graduates[i].university);
+}
+
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -59,11 +64,22 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+//graduates.map(element=> `${element.first_name} ${element.email}`);
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+
 const uni = [];
+//graduates.filter(element=> element.university.includes('Uni'));
+for (let i = 0; i < graduates.length; i++) {
+  if(graduates[i].university.includes('Uni')){
+    uni.push(graduates[i])
+  }
+}
 console.log(uni);
 
 
@@ -89,6 +105,7 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(element => animalNames.push(`Name: ${element.animal_name}, Scientific: ${element.scientific_name} `))
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -97,7 +114,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(element => element.animal_name.toLowerCase())
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -105,7 +122,7 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter(element => element.population < 5);
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -113,7 +130,7 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((accumulator,element)=> element.population+accumulator,0);
 console.log(populationTotal);
 
 
